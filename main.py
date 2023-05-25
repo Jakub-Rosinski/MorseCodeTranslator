d094dc193264f59206b14689d80c91f3348ad8c2
jakub-rosinski <jakub-rosinski@wp.pl>
2023-05-25 22:17:08 +0200

    parser for encoding and decoding

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+import argparse
+
 from translator.translator import Translator
 from coding import Encoder, Decoder
 
 
+def parser():
+    parse = argparse.ArgumentParser(prog="Morse Code Translator",
+                                    description='Translate Morse Code')
+    parse.add_argument("-e",
+                       "--encode",
+                       type=str,
+                       help="encode message")
+    parse.add_argument("-d",
+                       "--decode",
+                       type=str,
+                       help="decode message")
+
+    args = parse.parse_args()
+    return args
+
+
 def main() -> None:
 
-    e = Encoder("I'm Kuba and I'm from Poland")
-    msg = Translator(e)
-    print(msg.translate())
+    args = parser()
+    if text := args.encode:
+        e = Encoder(text)
+        msg = Translator(e)
+        print(f"Original message: {text}")
+        print(f"Morse code translation: {msg.translate()}")
 
-    d = Decoder(".. __ / _._ .._ _... ._ / ._ _. _.. / .. __ / .._. ._. ___ __ / .__. ___ ._.. ._ _. _..")
-    msg = Translator(d)
-    print(msg.translate())
+    if text := args.decode:
+        d = Decoder(text)
+        msg = Translator(d)
+        print(f"Original message: {text}")
+        print(f"Morse code translation: {msg.translate()}")
 
 
 if __name__ == '__main__':
